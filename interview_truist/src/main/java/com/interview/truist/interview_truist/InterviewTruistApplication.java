3811837984a8a2d0cf83a37d72428bb9ea98052f
Sandeep <Sandyb2711@gmail.com>
2025-01-08 21:57:00 +0530

    initial commit

---
+++ interview_truist/src/main/java/com/interview/truist/interview_truist/InterviewTruistApplication.java
@@ -0,0 +1,13 @@
+package com.interview.truist.interview_truist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewTruistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewTruistApplication.class, args);
+	}
+
+}
